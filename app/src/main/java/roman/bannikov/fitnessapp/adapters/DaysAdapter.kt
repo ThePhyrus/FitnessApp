1057a68cbc9d2fc8bf040d0ab351e0712bb12a2c
ThePhyrus <romaisgood@yandex.ru>
2022-08-04 00:54:57 +0300

    8 - Создал RecyclerViewAdapter (class DayHolder и компаратор с дифутилс)

--- app/src/main/java/roman/bannikov/fitnessapp/adapters/DaysAdapter.kt
+++ app/src/main/java/roman/bannikov/fitnessapp/adapters/DaysAdapter.kt
@@ -1,12 +1,15 @@
 package roman.bannikov.fitnessapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import roman.bannikov.fitnessapp.R
 import roman.bannikov.fitnessapp.databinding.DaysListItemBinding
 
-class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder> {
+class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder>(Comparator()) {
 
     class DayHolder(xmlDaysListItem: View) : RecyclerView.ViewHolder(xmlDaysListItem) {
         private val binding = DaysListItemBinding.bind(xmlDaysListItem)//FIXME не будет ли утечки?
@@ -16,6 +19,29 @@ class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder> {
             val exCounter = day.exercises.split(",").size.toString()
             tvExerciseCounter.text = exCounter
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayHolder {
+        val xmlView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.days_list_item, parent, false)
+        return DayHolder(xmlView)
+    }
+
+    //TODO сделай тут гадость, чтобы понять, как эта хрень работает
+
+    override fun onBindViewHolder(holder: DayHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
+
+    class Comparator : DiffUtil.ItemCallback<DayModel>() {
+        override fun areItemsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
 }
