47a4db2411cf70c44629a9a1ec9232f2c2ad6051
ThePhyrus <romaisgood@yandex.ru>
2022-08-10 22:50:11 +0300

    23 - Подкинул себе //тудух и //фихсним

--- app/src/main/java/roman/bannikov/fitnessapp/MainActivity.kt
+++ app/src/main/java/roman/bannikov/fitnessapp/MainActivity.kt
@@ -8,6 +8,9 @@ import roman.bannikov.fitnessapp.utils.FragmentManager
 
 //todo Добавить "сек." или "раз" к отображению названия следующего упражнения
 //todo Настроить tvWinner (шрифт и всякое такое)
+//todo Повыносить изменение текста ActionBar из onViewCreated() в какую-нибудь функцию?
+//FIXME Гифка следующего упражнения должна показываться во время расслабления и на экране подготовки
+//todo Найти кучу однообразных gif-картинок и подписать все упражнения
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
