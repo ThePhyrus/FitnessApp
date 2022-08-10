d2a9cbecea5f7641ad97edda42159f247fe245c8
ThePhyrus <romaisgood@yandex.ru>
2022-08-10 15:08:10 +0300

    20 - Добавил функцию определения типа упражнения (на время или на количество повторений), создал таймер упражнения.

--- app/src/main/java/roman/bannikov/fitnessapp/fragments/ExerciseFragment.kt
+++ app/src/main/java/roman/bannikov/fitnessapp/fragments/ExerciseFragment.kt
@@ -1,6 +1,7 @@
 package roman.bannikov.fitnessapp.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,13 +16,22 @@ import roman.bannikov.fitnessapp.adapters.ExerciseAdapter
 import roman.bannikov.fitnessapp.adapters.ExerciseModel
 import roman.bannikov.fitnessapp.databinding.FragmentExerciseBinding
 import roman.bannikov.fitnessapp.databinding.FragmentExerciseListBinding
+import roman.bannikov.fitnessapp.utils.COUNTDOWN_INTERVAL
+import roman.bannikov.fitnessapp.utils.COUNTDOWN_TIME
 import roman.bannikov.fitnessapp.utils.FragmentManager
+import roman.bannikov.fitnessapp.utils.TimeUtils
 
 class ExerciseFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ExerciseFragment()
+    }
 
     private var _binding: FragmentExerciseBinding? = null
     private val binding: FragmentExerciseBinding get() = _binding!!
     private var exerciseCounter: Int = 0
+    private var exerciseTimer: CountDownTimer? = null
     private var exerciseList: ArrayList<ExerciseModel>? = null
     private val viewModel: MainViewModel by activityViewModels()
 
@@ -39,31 +49,60 @@ class ExerciseFragment : Fragment() {
             exerciseList = it
             nextExercise()
         }
-        binding.btnNextExercise.setOnClickListener {  }
+        binding.btnNextExercise.setOnClickListener {
+            exerciseTimer?.cancel()
+            nextExercise()
+        }
     }
 
     private fun nextExercise() {
         if (exerciseCounter < exerciseList?.size!!) {
-            val exercise = exerciseList?.get(exerciseCounter++)
+            val exercise = exerciseList?.get(exerciseCounter++) ?: return
+            specifyExerciseType(exercise)
             showExercise(exercise)
         } else {
             Toast.makeText(activity, "Done", Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun showExercise(exercise: ExerciseModel?) = with(binding) {
-        if (exercise == null) return@with
+    private fun showExercise(exercise: ExerciseModel) = with(binding) {
         ivCurrentExercise.setImageDrawable(GifDrawable(root.context.assets, exercise.exerciseImage))
         tvExerciseName.text = exercise.exerciseName
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = ExerciseFragment()
+    private fun specifyExerciseType(exercise: ExerciseModel) {
+        if (exercise.exerciseTime.startsWith("x")) {
+            binding.tvExerciseTime.text = exercise.exerciseTime
+        } else {
+            startExerciseTimer(exercise)
+        }
+    }
+
+    private fun startExerciseTimer(exercise: ExerciseModel) = with(binding) {
+        progressBarExercise.max = exercise.exerciseTime.toInt() * 1000
+        exerciseTimer?.cancel()
+        exerciseTimer =
+            object : CountDownTimer(exercise.exerciseTime.toLong() * 1000, COUNTDOWN_INTERVAL) {
+                override fun onTick(millisUntilFinished: Long) {
+                    tvExerciseTime.text = TimeUtils.getTime(millisUntilFinished)
+                    progressBarExercise.progress = millisUntilFinished.toInt()
+                }
+
+                override fun onFinish() {
+                    nextExercise()
+                }
+            }.start()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        exerciseTimer?.cancel()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+
 }
